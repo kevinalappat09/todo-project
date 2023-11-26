bafe307aabc4807f595cb736287af3c6d53c839d
kevinalappat09 <kevin.d.alappat@gmail.com>
2023-11-26 19:14:09 +0530

    Added ESLint and Prettier and did something

--- src/createTodoElement.js
+++ src/createTodoElement.js
@@ -1,70 +1,64 @@
 function createTaskTitleUI(taskTitle) {
-    let newTaskTitle = document.createElement('div');
+    const newTaskTitle = document.createElement('div');
     newTaskTitle.innerHTML = taskTitle;
     return newTaskTitle;
 }
 
 function createTaskDueUI(taskDueDate) {
-    let newTaskDueDate = document.createElement('div');
+    const newTaskDueDate = document.createElement('div');
     newTaskDueDate.innerHTML = taskDueDate;
     return newTaskDueDate;
 }
 
 function createTaskDescription(taskDescription) {
-    let newTaskDescription = document.createElement('div');
+    const newTaskDescription = document.createElement('div');
     newTaskDescription.innerHTML = taskDescription;
     return newTaskDescription;
 }
 
 function createTaskPriority(taskPriority) {
-    let newTaskPriority = document.createElement('div');
-    if(taskPriority == 1) {
-        newTaskPriority.innerHTML = "High";
-    } else if(taskPriority == 2) {
-        newTaskPriority.innerHTML = "Medium";
+    const newTaskPriority = document.createElement('div');
+    if (taskPriority === 1) {
+        newTaskPriority.innerHTML = 'High';
+    } else if (taskPriority === 2) {
+        newTaskPriority.innerHTML = 'Medium';
     } else {
-        newTaskPriority.innerHTML = "Low";
+        newTaskPriority.innerHTML = 'Low';
     }
     return newTaskPriority;
 }
 
 function createTaskButtonGroup(uid) {
-    let newTaskButtonGroup = document.createElement('div');
-    
-    let deleteButton = document.createElement('button');
-    deleteButton.id = "task-del-button";
-    deleteButton.setAttribute('data-id',uid);
-    deleteButton.innerHTML = "Delete";
+    const newTaskButtonGroup = document.createElement('div');
+    const deleteButton = document.createElement('button');
+    deleteButton.id = 'task-del-button';
+    deleteButton.setAttribute('data-id', uid);
+    deleteButton.innerHTML = 'Delete';
     newTaskButtonGroup.appendChild(deleteButton);
 
-    let completeButton = document.createElement('button');
-    completeButton.innerHTML = "Complete";
-    completeButton.setAttribute('data-id',uid);
-    completeButton.id = "task-comp-button";
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = 'Complete';
+    completeButton.setAttribute('data-id', uid);
+    completeButton.id = 'task-comp-button';
     newTaskButtonGroup.appendChild(completeButton);
 
     return newTaskButtonGroup;
 }
 
-
 function createNewTaskDisplay(task) {
-    let newTask = document.createElement('div');
-    newTask.classList.add('task')
-        
+    const newTask = document.createElement('div');
+    newTask.classList.add('task');
     const newTaskTitle = createTaskTitleUI(task.getTitle());
     newTask.appendChild(newTaskTitle);
-    
     const newTaskDueDate = createTaskDueUI(task.getDueDate());
     newTask.appendChild(newTaskDueDate);
-
     const newTaskDescription = createTaskDescription(task.getDescription());
     newTask.appendChild(newTaskDescription);
-
     const newTaskPriority = createTaskPriority(task.getPriority());
     newTask.appendChild(newTaskPriority);
 
-    if(task.getIsComplete() == true) {
-        newTask.style.color = "red";
+    if (task.getIsComplete() === true) {
+        newTask.style.color = 'red';
     }
 
     const buttonGroup = createTaskButtonGroup(task.getUID());
@@ -73,4 +67,4 @@ function createNewTaskDisplay(task) {
     return newTask;
 }
 
-export {createNewTaskDisplay};
+export default { createNewTaskDisplay };
--- src/createUIElement.js
+++ src/createUIElement.js
@@ -1,163 +1,175 @@
 import { projectListInstance } from "./projectList";
 
 class UIGen {
-    constructor() {
 
+  static createButton(idName, text) {
+    const newButton = document.createElement("button");
+    newButton.classList.add("button");
+    newButton.id = idName;
+    newButton.innerHTML = text;
+    return newButton;
+  }
+
+  static createLinkButton(idName, text) {
+    const newButton = document.createElement("button");
+    newButton.classList.add("link-button");
+    newButton.id = idName;
+    newButton.innerHTML = text;
+    return newButton;
+  }
+
+  static createContainerDiv(idName) {
+    const newContainerDiv = document.createElement("div");
+    newContainerDiv.classList.add("container");
+    newContainerDiv.id = idName;
+    return newContainerDiv;
+  }
+
+  static createGeneralDiv(className, idName) {
+    const newGeneralDiv = document.createElement("div");
+    newGeneralDiv.classList.add(className);
+    newGeneralDiv.id = idName;
+    return newGeneralDiv;
+  }
+
+  static createInput(id, type, placeholder) {
+    const newInput = document.createElement("input");
+    newInput.classList.add("form-input");
+    newInput.id = id;
+    newInput.type = type;
+    if (placeholder) {
+      newInput.placeholder = placeholder;
+    }
+    return newInput;
+  }
+
+  static createPrioritySelector() {
+    const taskPriorityInput = document.createElement("select");
+    taskPriorityInput.id = "priority-input";
+    taskPriorityInput.classList.add("form-input");
+
+    const taskPriorityOption1 = document.createElement("option");
+    taskPriorityOption1.value = 1;
+    taskPriorityOption1.innerHTML = "High";
+    taskPriorityInput.appendChild(taskPriorityOption1);
+
+    const taskPriorityOption2 = document.createElement("option");
+    taskPriorityOption2.value = 2;
+    taskPriorityOption2.innerHTML = "Medium";
+    taskPriorityInput.appendChild(taskPriorityOption2);
+
+    const taskPriorityOption3 = document.createElement("option");
+    taskPriorityOption3.value = 3;
+    taskPriorityOption3.innerHTML = "Low";
+    taskPriorityInput.appendChild(taskPriorityOption3);
+
+    return taskPriorityInput;
+  }
+
+  static createProjectSelector() {
+    const projectSelector = document.createElement("select");
+    projectSelector.classList.add("form-input");
+    projectSelector.classList.add("selector");
+    projectSelector.name = "project-selector";
+    projectSelector.id = "project-input";
+
+    const projects = Array.from(projectListInstance.getProjectList());
+    for (const i = 0; i < projects.length; i++) {
+      const projectOption = document.createElement("option");
+      projectOption.value = projects[i].getProjectName();
+      projectOption.innerHTML = projects[i].getProjectName();
+      projectSelector.appendChild(projectOption);
     }
 
-    createButton(idName, text) {
-        let newButton = document.createElement('button');
-        newButton.classList.add('button');
-        newButton.id = idName;
-        newButton.innerHTML = text;
-        return newButton;
-    }
-    
-    createLinkButton(idName,text) {
-        let newButton = document.createElement('button');
-        newButton.classList.add('link-button');
-        newButton.id = idName;
-        newButton.innerHTML = text;
-        return newButton;
-    }
+    return projectSelector;
+  }
 
-    createContainerDiv(idName) {
-        let newContainerDiv = document.createElement('div');
-        newContainerDiv.classList.add('container');
-        newContainerDiv.id = idName;
-        return newContainerDiv;
-    }
+  static createColorSelector() {
+    const projectColorInput = document.createElement("select");
+    projectColorInput.id = "pColor-input";
+    projectColorInput.classList.add("form-input");
 
-    createGeneralDiv(className,idName) {
-        let newGeneralDiv = document.createElement('div');
-        newGeneralDiv.classList.add(className);
-        newGeneralDiv.id = idName;
-        return newGeneralDiv;
-    }
+    const pco1 = document.createElement("option");
+    pco1.value = "blue";
+    pco1.innerHTML = "Blue";
+    projectColorInput.appendChild(pco1);
 
-    createInput(id,type,placeholder) {
-        let newInput = document.createElement('input');
-        newInput.classList.add('form-input');
-        newInput.id = id;
-        newInput.type = type;
-        if(placeholder) {
-            newInput.placeholder = placeholder;
-        }
-        return newInput;
-    }
+    const pco2 = document.createElement("option");
+    pco2.value = "green";
+    pco2.innerHTML = "Green";
+    projectColorInput.appendChild(pco2);
 
-    createPrioritySelector() {
-        let taskPriorityInput = document.createElement('select');
-        taskPriorityInput.id = 'priority-input';
-        taskPriorityInput.classList.add('form-input');
+    const pco3 = document.createElement("option");
+    pco3.value = "red";
+    pco3.innerHTML = "Red";
+    projectColorInput.appendChild(pco3);
 
-        let taskPriorityOption1 = document.createElement('option');
-        taskPriorityOption1.value = 1;
-        taskPriorityOption1.innerHTML = "High";
-        taskPriorityInput.appendChild(taskPriorityOption1);
-
-        let taskPriorityOption2 = document.createElement('option');
-        taskPriorityOption2.value = 2;
-        taskPriorityOption2.innerHTML = "Medium";
-        taskPriorityInput.appendChild(taskPriorityOption2);
-
-        let taskPriorityOption3 = document.createElement('option');
-        taskPriorityOption3.value = 3;
-        taskPriorityOption3.innerHTML = "Low";
-        taskPriorityInput.appendChild(taskPriorityOption3);
-
-        return taskPriorityInput;
-    }
-
-    createProjectSelector() {
-        let projectSelector = document.createElement('select');
-        projectSelector.classList.add('form-input');
-        projectSelector.classList.add('selector');
-        projectSelector.name = "project-selector";
-        projectSelector.id = 'project-input';
-
-        const projects = Array.from(projectListInstance.getProjectList());
-        for(let i=0; i<projects.length;i++) {
-            let projectOption = document.createElement('option');
-            projectOption.value = projects[i].getProjectName();
-            projectOption.innerHTML = projects[i].getProjectName();
-            projectSelector.appendChild(projectOption);
-        }
-
-        return projectSelector;
-    }
-
-    createColorSelector() {
-        let projectColorInput = document.createElement('select');
-        projectColorInput.id = 'pColor-input';
-        projectColorInput.classList.add('form-input');
-        
-        let pco1 = document.createElement('option');
-        pco1.value = "blue";
-        pco1.innerHTML = "Blue";
-        projectColorInput.appendChild(pco1);
-
-        let pco2 = document.createElement('option');
-        pco2.value = "green";
-        pco2.innerHTML = "Green";
-        projectColorInput.appendChild(pco2);
-
-        let pco3 = document.createElement('option');
-        pco3.value = "red";
-        pco3.innerHTML = "Red";
-        projectColorInput.appendChild(pco3);
-
-        return projectColorInput;
-    }
+    return projectColorInput;
+  }
 }
 
-const uiGenInstance = new UIGen;
+const uiGenInstance = new UIGen();
 
 class FormGen {
-    constructor() {
+  constructor() {}
 
-    }
+  createTaskForm() {
+    const taskFormDiv = document.createElement("div");
+    taskFormDiv.classList.add("form-div");
 
-    createTaskForm() {
-        let taskFormDiv = document.createElement('div');
-        taskFormDiv.classList.add('form-div');
-    
-        let taskTitleInput = uiGenInstance.createInput('title-input','text',"Title");
-        taskFormDiv.appendChild(taskTitleInput);
-    
-        let taskDueInput = uiGenInstance.createInput('due-input','date');
-        taskFormDiv.appendChild(taskDueInput);
-    
-        let taskDescriptionInput = uiGenInstance.createInput('desc-input','text','Description');
-        taskFormDiv.appendChild(taskDescriptionInput);
-    
-        
-        taskFormDiv.appendChild(uiGenInstance.createPrioritySelector());
-    
-        let taskProjectInput = uiGenInstance.createProjectSelector();
-        taskFormDiv.appendChild(taskProjectInput);
-    
-        let taskCreateButton = uiGenInstance.createButton('create-task-button',"Create");
-        taskFormDiv.appendChild(taskCreateButton);
-    
-        return taskFormDiv;
-    }
+    const taskTitleInput = uiGenInstance.createInput(
+      "title-input",
+      "text",
+      "Title"
+    );
+    taskFormDiv.appendChild(taskTitleInput);
 
-    createProjectForm() {
-        let projectForm = document.createElement('div');
-        projectForm.classList.add('form-div');
-    
-        let projectNameInput = uiGenInstance.createInput('pName-input','text','Project Name');
-        projectForm.appendChild(projectNameInput);
-    
-        projectForm.appendChild(uiGenInstance.createColorSelector());
-    
-        let projectCreateButton = uiGenInstance.createButton('create-project-button','Create Project');
-        projectForm.appendChild(projectCreateButton);
-    
-        return projectForm;
-    }
+    const taskDueInput = uiGenInstance.createInput("due-input", "date");
+    taskFormDiv.appendChild(taskDueInput);
+
+    const taskDescriptionInput = uiGenInstance.createInput(
+      "desc-input",
+      "text",
+      "Description"
+    );
+    taskFormDiv.appendChild(taskDescriptionInput);
+
+    taskFormDiv.appendChild(uiGenInstance.createPrioritySelector());
+
+    const taskProjectInput = uiGenInstance.createProjectSelector();
+    taskFormDiv.appendChild(taskProjectInput);
+
+    const taskCreateButton = uiGenInstance.createButton(
+      "create-task-button",
+      "Create"
+    );
+    taskFormDiv.appendChild(taskCreateButton);
+
+    return taskFormDiv;
+  }
+
+  createProjectForm() {
+    const projectForm = document.createElement("div");
+    projectForm.classList.add("form-div");
+
+    const projectNameInput = uiGenInstance.createInput(
+      "pName-input",
+      "text",
+      "Project Name"
+    );
+    projectForm.appendChild(projectNameInput);
+
+    projectForm.appendChild(uiGenInstance.createColorSelector());
+
+    const projectCreateButton = uiGenInstance.createButton(
+      "create-project-button",
+      "Create Project"
+    );
+    projectForm.appendChild(projectCreateButton);
+
+    return projectForm;
+  }
 }
 
-const formGenInstance = new FormGen;
-export {uiGenInstance, formGenInstance };
+const formGenInstance = new FormGen();
+export { uiGenInstance, formGenInstance };
